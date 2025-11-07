56b4fcdfd351f2aacba20bbc49e15346812c4208
Arnaldo Agreda <arnaldo.agreda@galac.com>
2025-11-07 13:48:07 -0400

    Capitulo 5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+//   weight: ["400", "700"],
+//   variable: "--font-montserrat",
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+//   variable: "--font-lusitana",
+});
